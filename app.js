2c7b8872e2771b9aaed145e1517db66c45a93acd
ZeroDayOutplay <hama.tm662@gmail.com>
2022-06-09 05:40:58 +0100

    db 1st commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { type } = require("express/lib/response");
+const fs = require("fs");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -31,7 +31,21 @@ app.post("/sign_in.html", (req, res) => {
     const email = req.body.email;
     if (email === "") res.sendFile(__dirname + "/invalid_u_pwd.html");
     else res.sendFile(sign_in);
+    check_mail(email);
 });
 
 app.listen(3000, () => {
 });
+
+
+
+
+
+function check_mail(mail){
+    const alt = mail.split("@");
+    const dor = mail.split(".");
+    if (alt.length != 2 || dor.length != 2) return "Invalid Email";
+    fs.readFile("db.txt", "utf8", (err, data) => {
+        console.log(data);
+    });
+}
