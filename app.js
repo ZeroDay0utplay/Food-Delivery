6168154c36886b0a0fcc9f85b61298f5223cce31
ZeroDayOutplay <hama.tm662@gmail.com>
2022-06-09 05:31:15 +0100

    Add Error msg

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ app.get("/sign_up.html", (req, res) => {
 
 app.post("/sign_up.html", (req, res) => {
     const email = req.body.email;
-    if (email === "") res.send("ERROR !!!");
+    if (email === "") res.sendFile(__dirname + "/email_exist.html");
     else res.sendFile(sign_up);
+});
+
+app.post("/sign_in.html", (req, res) => {
+    const email = req.body.email;
+    if (email === "") res.sendFile(__dirname + "/invalid_u_pwd.html");
+    else res.sendFile(sign_in);
 });
 
 app.listen(3000, () => {
