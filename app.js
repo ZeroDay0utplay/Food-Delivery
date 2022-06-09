197ed48109aea6bf40c54b8dba1269f0d6d502f8
ZeroDayOutplay <hama.tm662@gmail.com>
2022-06-09 04:57:13 +0100

    add listener

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { type } = require("express/lib/response");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const sign_up = __dirname + "/sign_up.html";
+const sign_in = __dirname + "/sign_in.html";
+
+app.get("/", (req, res) => {
+    res.sendFile(sign_in);
+});
+
+app.get("/sign_in.html", (req, res) => {
+    res.sendFile(sign_in);
+})
+
+app.get("/sign_up.html", (req, res) => {
+    res.sendFile(sign_up);
+})
+
+app.post("/sign_up.html", (req, res) => {
+    const email = req.body.email;
+    if (email === "") res.send("ERROR !!!");
+    else res.sendFile(sign_up);
+});
+
+app.listen(3000, () => {
+});
