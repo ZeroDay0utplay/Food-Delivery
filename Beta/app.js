afe13067d2da3e6a252612b753fe61da09c96ad0
ZeroDayOutplay <hama.tm662@gmail.com>
2022-07-10 03:24:10 +0100

    tiny changes

--- Beta/app.js
+++ Beta/app.js
@@ -1,73 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const fs = require("fs");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-const sign_up = __dirname + "/sign_up.html";
-const sign_in = __dirname + "/sign_in.html";
+const sign_up = __dirname + "/views/sign_up.ejs";
+const sign_in = __dirname + "/views/sign_in.ejs";
+
+let login_msg = "";
+let su_msg = "";
 
 app.get("/", (req, res) => {
-    res.sendFile(sign_in);
-});
+    res.render("sign_in", {login: ""});
+})
 
 app.get("/sign_in.html", (req, res) => {
-    res.sendFile(sign_in);
+    res.render("sign_in", {login: ""});
 })
 
 app.get("/sign_up.html", (req, res) => {
-    res.sendFile(sign_up);
+    res.render(sign_up, {login: ""});
 })
 
 app.post("/sign_up.html", (req, res) => {
     const email = req.body.email;
-    if (email === "") res.sendFile(__dirname + "/email_exist.html");
-    else res.sendFile(sign_up);
+    const password = req.body.pwd;
+    console.log(email);
+    console.log(password);
+    if (email === "med@tn.cox" && password === "zdo2022") su_msg = "success";
+    else su_msg = "used_email";
+    console.log(su_msg);
+    res.render("sign_up", {login: su_msg});
 });
 
 app.post("/sign_in.html", (req, res) => {
     const email = req.body.email;
-    const emailChecked = check_mail(email);
-    if (emailChecked == -1) res.sendFile(__dirname + "/invalid_u_pwd.html");
-    else if (emailChecked == 1){
-        
-    }
-    else if (emailChecked == 0){
-        
-    }
+    const password = req.body.pwd;
+    if (email === "med@tn.cox" && password === "zdo2022") login_msg = "success";
+    else login_msg = "wrongpwd";
+    res.render("sign_in", {login: login_msg});
 });
 
 app.listen(process.env.PORT || 3000, () => {
 });
-
-
-
-
-
-
-
-
-function str2list(str){
-    const lst = str.split("\n");
-    for (var i=0; i<lst.length; i++){
-        lst[i] = lst[i].trim("\r");
-    }
-    return lst;
-}
-
-
-
-function check_mail(mail){
-    const alt = mail.split("@");
-    const dot = mail.split(".");
-    if (alt != 2 || dot != 2) return -1;
-    fs.readFile(__dirname + "/db.txt", "utf8", (err, data) => {
-        const usrs = str2list(data);
-        for (var i=0; i<usrs.length; i++){
-            if (usrs[i] === mail) return 1;
-        }
-        return 0;
-    });
-}
