9f7792772fbe73b627b0af1765cd9ede1281d68f
ZeroDayOutplay <hama.tm662@gmail.com>
2022-07-25 23:48:40 +0100

    Add Product Section

--- Beta/app.js
+++ Beta/app.js
@@ -10,6 +10,7 @@ app.set("view engine", "ejs");
 
 const sign_up = __dirname + "/views/sign_up.ejs";
 const sign_in = __dirname + "/views/sign_in.ejs";
+const product = __dirname = "product.ejs";
 
 let login_msg = "";
 let su_msg = "";
@@ -26,22 +27,15 @@ app.get("/sign_up.html", (req, res) => {
     res.render(sign_up, {login: ""});
 })
 
+app.get("/product", (req, res) => {
+    res.render(product, {produit: "Makloub"});
+})
+
 app.post("/sign_up.html", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.pwd;
-    console.log(email);
-    console.log(password);
-    if (email === "med@tn.cox" && password === "zdo2022") su_msg = "success";
-    else su_msg = "used_email";
-    console.log(su_msg);
     res.render("sign_up", {login: su_msg});
 });
 
 app.post("/sign_in.html", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.pwd;
-    if (email === "med@tn.cox" && password === "zdo2022") login_msg = "success";
-    else login_msg = "wrongpwd";
     res.render("sign_in", {login: login_msg});
 });
 
---
+++ heroku_pusher.py
@@ -0,0 +1,9 @@
+import os
+
+
+os.system("heroku login")
+os.system("git init")
+os.system("git add .")
+os.system("git commit -m '" + input("[+] Commit Message : ") + '"')
+os.system("heroku create")
+os.system("git push heroku master")
